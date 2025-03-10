a788e641fdd88e70a5a08165498ddd8e8c50eec9
Legom5 <19olegka97@mail.ru>
2025-03-10 13:12:58 +0300

    update activity_main.xml

--- app/src/main/java/legom/handbagshop/presentation/fragments/ProfileFragment.kt
+++ app/src/main/java/legom/handbagshop/presentation/fragments/ProfileFragment.kt
@@ -1,16 +1,13 @@
 package legom.handbagshop.presentation.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import legom.handbagshop.R
 
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
